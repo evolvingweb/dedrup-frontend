2fb4512c449939136c0d4beb79caf25ff252b175
Don Lalicon <don.lalicon@evolvingweb.ca>
2026-03-02 22:08:23 -0500

    - spacing tweaks

--- components/drupal/RichText.tsx
+++ components/drupal/RichText.tsx
@@ -4,7 +4,7 @@ interface RichTextProps {
 
 export function RichText({ body }: RichTextProps) {
   return (
-    <section className="flex flex-col items-center px-page-x py-section-lg w-full">
+    <section className="flex flex-col items-center px-page-x py-10 w-full">
       <div
         dangerouslySetInnerHTML={{ __html: body }}
         className="prose max-w-container w-full"
